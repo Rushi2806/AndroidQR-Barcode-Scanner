9750d2eba32ed178a574ae3af4f1ccb48352f1ea
Rushikesh Borkar <rushikeshborkar97@gmail.com>
2024-01-02 09:01:48 +0530

    This is QR & Barcode Scanner

--- app/src/main/java/com/rushiborkar/qrscanner/MainActivity.java
+++ app/src/main/java/com/rushiborkar/qrscanner/MainActivity.java
@@ -1,37 +1,26 @@
 package com.rushiborkar.qrscanner;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity
     {
 
-   ImageButton imageButton3;
 
-
-
-
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View ImageButton = findViewById(R.id.imageButton3);
         ImageButton.setOnClickListener(v->
-        {
-            scanCode();
-
-        });
+                scanCode());
     }
 
     private void scanCode()
@@ -50,14 +39,7 @@ public class MainActivity extends AppCompatActivity
         if(result.getContents()!=null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i)
-                {
-                    dialogInterface.dismiss();
-                }
-
-            }).show();
+            builder.setPositiveButton("OK", (dialogInterface, i) -> dialogInterface.dismiss()).show();
         }
     });
 
